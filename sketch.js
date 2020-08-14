7fac98e5dd1c8ee9d830650a22a0c2a6f6721836
NamanKumar28 <godrocknamankk@gmail.com>
2020-08-14 16:56:28 +0530

    CarRacing

--- sketch.js
+++ sketch.js
@@ -1,29 +1,54 @@
-var ball;
+var ball,database;
+var position;
 
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var dbBallPosition = database.ref('ball/position');
+    dbBallPosition.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+    if(position !== undefined){
+
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-5,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(5,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-5);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+5);
+        }
+        drawSprites();
     }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
+       
 }
 
 function changePosition(x,y){
     ball.x = ball.x + x;
     ball.y = ball.y + y;
+}
+
+function showError(){
+    console.log("errorReading");
+}
+
+function readPosition(data){
+position = data.val();
+ball.x = position.x;
+ball.y = position.y;
+}
+
+function writePosition(x,y){
+database.ref('ball/position').set({
+    'x': position.x + x,
+    'y': position.y + y
+})
 }
